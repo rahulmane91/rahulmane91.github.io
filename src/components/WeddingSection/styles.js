c63218c66ff9b2c13e11c77d8ad0ad5b4dd027d4
Rahul Mane <rahul.mane@e-zest.in>
2020-06-17 22:06:31 +0530

    removing

--- src/components/WeddingSection/styles.js
+++
@@ -1,9 +0,0 @@
-import { css } from '@emotion/core';
-import Background from '@assets/images/img_bg_3.jpg';
-
-export const styWrapper = css`
-  background-image: url(${Background});
-  background-size: cover;
-  width: 100%;
-  min-height: 70vh;
-`;

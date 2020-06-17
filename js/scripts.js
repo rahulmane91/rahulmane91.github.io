264bf1e1c5eae39664a22e78d244f06eb266ad31
Rahul Mane <rahul.mane@e-zest.in>
2020-06-17 23:09:33 +0530

    added timer

--- js/scripts.js
+++ js/scripts.js
@@ -43,3 +43,18 @@
   
   })(jQuery); // End of use strict
   
+var countDownDate = new Date("June 30, 2020 12:30:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("weddingTimer").innerHTML = days + " Days | " + hours + " Hours | "
+  + minutes + " Minutes | " + seconds + " Seconds ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
